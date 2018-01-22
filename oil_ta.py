ffac850c9d114a7adcf16f87921e78c5f3f59c4d
suesoo <suesoo73@gmail.com>
2018-01-22 19:04:23 +0900

    init ver 1.02 20일 macd 등 장기 지표 추가

--- oil_ta.py
+++ oil_ta.py
@@ -30,28 +30,41 @@ def ta_analizer(b_data):
     :param b_data: dataframe including price & volume
     :return: dataframe including technical analysis with price & volume
     '''
-    macd, macdsignal, macdhist = ta.MACD(b_data['price'].as_matrix(), fastperiod=12, slowperiod=26, signalperiod=9)
+    macd_5, macdsignal_5, macdhist_5 = ta.MACD(b_data['price'].as_matrix(), fastperiod=5, slowperiod=15, signalperiod=4)
+    macd_20, macdsignal_20, macdhist_20 = ta.MACD(b_data['price'].as_matrix(), fastperiod=20, slowperiod=60, signalperiod=16)
+    sma_5 = ta.SMA(b_data['price'].as_matrix(), timeperiod=5)
+    sma_20 = ta.SMA(b_data['price'].as_matrix(), timeperiod=20)
+
     rsi = ta.RSI(b_data['price'].as_matrix())
-    b_data['macd'] = macd
-    b_data['macdsignal'] = macdsignal
-    b_data['macdhist'] = macdhist
+    b_data['sma_5'] = sma_5
+    b_data['sma_20'] = sma_20
+    b_data['macd_5'] = macd_5
+    b_data['macdsignal_5'] = macdsignal_5
+    b_data['macdhist_5'] = macdhist_5
+    b_data['macd_20'] = macd_20
+    b_data['macdsignal_20'] = macdsignal_20
+    b_data['macdhist_20'] = macdhist_20
+
     b_data['rsi'] = rsi
     # b_data['d_price'] = b_data['price'].diff(5)
-    b_data['pct5_price'] = b_data['price'].pct_change(5) *100
+    b_data['pct5_sma_5'] = b_data['sma_5'].pct_change(5) *100
+    b_data['pct5_sma_5'] = b_data['pct5_sma_5'].shift(-5)
+    b_data['pct5_price'] = b_data['price'].pct_change(5) * 100
     b_data['pct5_price'] = b_data['pct5_price'].shift(-5)
-    b_data['pct5_macd'] = b_data['macd'].diff(5)/b_data['price'] *100
-    b_data['pct5_macdsignal'] = b_data['macdsignal'].diff(5)/b_data['price'] *100
-    b_data['pct5_macdhist'] = b_data['macdhist'].diff(5)/b_data['price'] *100
-    b_data['pct5_rsi'] = b_data['rsi'].diff(5)/b_data['price'] *100
-    b_data['pct5_vol'] = b_data['vol'].pct_change(5)
+    b_data['pct5_macd_5'] = b_data['macd_5'].diff(5)/b_data['price'] *100
+    b_data['pct5_macdsignal_5'] = b_data['macdsignal_5'].diff(5)/b_data['price'] *100
+    b_data['pct5_macdhist_5'] = b_data['macdhist_5'].diff(5)/b_data['price'] *100
+    b_data['d5_rsi'] = b_data['rsi'].diff(5)
+    b_data['pct5_vol'] = b_data['vol'].pct_change(5)*100
+
     print b_data
-    b_data['pct20_price'] = b_data['price'].pct_change(5) *100
-    b_data['pct20_price'] = b_data['pct20_price'].shift(-20)
-    b_data['pct20_macd'] = b_data['macd'].diff(20)/b_data['price'] *100
-    b_data['pct20_macdsignal'] = b_data['macdsignal'].diff(20)/b_data['price'] *100
-    b_data['pct20_macdhist'] = b_data['macdhist'].diff(20)/b_data['price'] *100
-    b_data['pct20_rsi'] = b_data['rsi'].diff(20)/b_data['price'] *100
-    b_data['pct20_vol'] = b_data['vol'].pct_change(20)
+    b_data['pct5_sma_20'] = b_data['sma_20'].pct_change(5) *100
+    b_data['pct5_sma_20'] = b_data['pct5_sma_20'].shift(-5)
+    b_data['pct5_macd_20'] = b_data['macd_20'].diff(5) /b_data['price']* 100
+    b_data['pct5_macdsignal_20'] = b_data['macdsignal_20'].diff(5) /b_data['price'] * 100
+    b_data['pct5_macdhist_20'] = b_data['macdhist_20'].diff(5) /b_data['price']* 100
+    # b_data['d20_rsi'] = b_data['rsi'].diff(20)
+    # b_data['pct20_vol'] = b_data['vol'].pct_change(20)
     return b_data
 
 b_data = load_data('oilpricedata.txt')
